7d639841b317dc3047714330ba50e472238109d3
Ladiarus <yarlad03@gmail.com>
2020-08-11 22:10:50 +0300

    ball

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * 200, ForceMode.Force);
+        rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * 200, ForceMode.Force);
+    }
+}
---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject opora, player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        opora = GameObject.Find("Opora");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        opora.transform.position = player.transform.position;
+    }
+}
